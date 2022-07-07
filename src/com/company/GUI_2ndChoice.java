49f2e6d9387018ec960987547e91a77ace2dcc00
Eddie Rawile <edward.rawile@gmail.com>
2022-07-07 10:15:47 +0100

    10 buttons

--- src/com/company/GUI_2ndChoice.java
+++ src/com/company/GUI_2ndChoice.java
@@ -14,6 +14,16 @@ public class GUI_2ndChoice implements ActionListener {
     private static JPasswordField passwordText;
     private static JButton button1;
     private static JButton button2;
+    private static JButton button3;
+    private static JButton button4;
+    private static JButton button5;
+    private static JButton button6;
+    private static JButton button7;
+    private static JButton button8;
+    private static JButton button9;
+    private static JButton button10;
+
+
     private static JLabel success;
 
     public GUI_2ndChoice(){
@@ -26,28 +36,64 @@ public class GUI_2ndChoice implements ActionListener {
 
         panel.setLayout(null);
 
-        userLabel = new JLabel("Thank you for logging in!");// JLabel is text on the window
+
+        userLabel = new JLabel("Individual Team information");// JLabel is text on the window
         userLabel.setBounds(310,10,550,100);
         panel.add(userLabel);// you have to add everything to panel
 
-        userLabel2 = new JLabel("What would you like to do?");// JLabel is text on the window
-        userLabel2.setBounds(310,40,550,100);
+        userLabel2 = new JLabel("Team:");// JLabel is text on the window
+        userLabel2.setBounds(370,40,550,100);
         panel.add(userLabel2);// you have to add everything to panel
 
-        button1 = new JButton("Specific League Table");// creates a button with text
-        button1.setBounds(310,160,200,50);
+        button1 = new JButton("Fleetdown United");// creates a button with text
+        button1.setBounds(100,160,150,50);
         button1.addActionListener(this);//If button is clicked, runs actionPerformed
         panel.add(button1);
 
-        button2 = new JButton("Individual Team Information");// creates a button with text
-        button2.setBounds(310,240,200,50);
-        button2.addActionListener(this);//If button is clicked, runs actionPerformed
+        button2 = new JButton("Northfleet Eagles");// creates a button with text
+        button2 .setBounds(250,160,150,50);
+        button2 .addActionListener(this);//If button is clicked, runs actionPerformed
         panel.add(button2);
 
+        button3 = new JButton("Belvedere Youth");// creates a button with text
+        button3.setBounds(400,160,150,50);
+        button3.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button3);
 
-        success = new JLabel("");
-        success.setBounds(10,110,300,25);
-        panel.add(success);
+        button4 = new JButton("Parkwood Rangers Green");// creates a button with text
+        button4.setBounds(550,160,150,50);
+        button4.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button4);
+
+        button5 = new JButton("Dulwich Village White");// creates a button with text
+        button5.setBounds(100,250,150,50);
+        button5.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button5);
+
+        button6 = new JButton("Danson Sports Black");// creates a button with text
+        button6.setBounds(250,250,150,50);
+        button6.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button6);
+
+        button7 = new JButton("Chislehurst Wanderers");// creates a button with text
+        button7.setBounds(400,250,150,50);
+        button7.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button7);
+
+        button8 = new JButton("Swanscombe Tigers");// creates a button with text
+        button8.setBounds(550,250,150,50);
+        button8.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button8);
+
+        button9 = new JButton("Junior Reds Sabres");// creates a button with text
+        button9.setBounds(100,340,150,50);
+        button9.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button9);
+
+        button10 = new JButton("Welling Wanderers");// creates a button with text
+        button10.setBounds(250,340,150,50);
+        button10.addActionListener(this);//If button is clicked, runs actionPerformed
+        panel.add(button10);
 
 
         frame.setVisible(true);
